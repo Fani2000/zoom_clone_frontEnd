c86276a54a84a71e48b937d1b679cf953efa821f
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-08-19 12:16:20 +0200

    Added the MenuButtons

--- components/MenuButtons.js
+++ components/MenuButtons.js
@@ -1,10 +1,69 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+const items = [
+  {
+    id: 1,
+    name: "video-camera",
+    title: "New Meeting",
+    color: "orange",
+  },
+  {
+    id: 2,
+    name: "plus-square",
+    title: "Join",
+    color: "blue",
+  },
+  {
+    id: 3,
+    name: "calendar",
+    title: "Schedule",
+    color: "blue",
+  },
+  {
+    id: 4,
+    name: "upload",
+    title: "Share Screen",
+    color: "blue",
+  },
+];
 
 const MenuButtons = () => {
   return (
-    <View>
-      <Text>MenuButtons</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: 10,
+      }}
+    >
+      {items.map((item) => {
+        return (
+          <View
+            key={item.id}
+            style={{ flexDirection: "column", alignItems: "center" }}
+          >
+            <TouchableOpacity
+              style={{
+                backgroundColor: item.color,
+                padding: 10,
+                borderRadius: 10,
+                height: 50,
+                width: 50,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <FontAwesome name={item.name} size={23} color={"white"} />
+            </TouchableOpacity>
+            <Text style={{ color: "#f1f1f1", marginTop: 10 }}>
+              {item.title}
+            </Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
